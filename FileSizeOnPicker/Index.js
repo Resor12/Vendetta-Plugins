1d9d447e08b2fae6b01e2992409b8b6e93ef7b9e
Resor12 <77066182+Resor12@users.noreply.github.com>
2025-10-02 12:21:31 +0200

    Create Index.js

---
+++ FileSizeOnPicker/Index.js
@@ -0,0 +1,36 @@
+import { after } from "@vendetta/patcher";
+import { findByName } from "@vendetta/metro";
+
+const FilePickerRow = findByName("FilePickerRow", false);
+
+let patch;
+
+export default {
+  onLoad: () => {
+    patch = after("default", FilePickerRow, (args, res) => {
+      try {
+        const file = args[0]?.file;
+        if (!file) return res;
+
+        const size = formatFileSize(file.size);
+
+        // dodajemy rozmiar obok nazwy
+        if (res?.props?.children?.props?.children) {
+          res.props.children.props.children += ` (${size})`;
+        }
+      } catch (e) {
+        console.error("[FileSizeOnPicker]", e);
+      }
+      return res;
+    });
+  },
+  onUnload: () => {
+    patch?.();
+  },
+};
+
+function formatFileSize(bytes) {
+  if (bytes < 1024) return `${bytes} B`;
+  if (bytes < 1024 * 1024) return `${(bytes / 1024).toFixed(1)} KB`;
+  return `${(bytes / (1024 * 1024)).toFixed(1)} MB`;
+}
