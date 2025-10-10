cdf8b93068ed6898f6b4a49cf611f8d2e8757265
Resor12 <77066182+Resor12@users.noreply.github.com>
2025-10-10 21:42:18 +0200

    Update index.js

--- FileSizeOnPicker/index.js
+++ FileSizeOnPicker/index.js
@@ -34,3 +34,5 @@ function formatFileSize(bytes) {
   if (bytes < 1024 * 1024) return `${(bytes / 1024).toFixed(1)} KB`;
   return `${(bytes / (1024 * 1024)).toFixed(1)} MB`;
 }
+// @ sourceType=module
+
