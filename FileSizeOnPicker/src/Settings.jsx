19716d326def916ee63bafb871035b438fd4747e
Resor12 <77066182+Resor12@users.noreply.github.com>
2025-10-31 23:39:19 +0100

    Delete FileSizeOnPicker directory

--- FileSizeOnPicker/src/Settings.jsx
+++
@@ -1,9 +0,0 @@
-import { Forms } from "@revenge/ui/components";
-const { FormText } = Forms;
-
-export default () => (
-    // TODO: add configurable scale 
-    <FormText>
-        Hello, world!
-    </FormText>
-)
