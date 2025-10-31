c46949d77e9fc3d6f493b1684499f55a471e876b
Resor12 <77066182+Resor12@users.noreply.github.com>
2025-10-31 23:18:51 +0100

    Integrate FormText component in Settings
    Added FormText component to Settings.tsx.

--- FileSizeOnPicker/src/Settings.tsx
+++ FileSizeOnPicker/src/Settings.tsx
@@ -1,1 +1,9 @@
+import { Forms } from "@revenge/ui/components";
+const { FormText } = Forms;
 
+export default () => (
+    // TODO: add configurable scale 
+    <FormText>
+        Hello, world!
+    </FormText>
+)
